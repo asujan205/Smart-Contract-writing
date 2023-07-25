2d9f8eb0ad3ad70965597b3806d9a46b2296e5a1
asujan205 <asujan205@gmail.com>
2023-07-25 10:40:44 +0545

    #datatypes

---
+++ Datatype.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+
+contract DataType{
+
+    address public myAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    uint public myUint = 1;
+    uint256 public myUint256 = 1;
+    uint8 public myUint8 = 1;
+    string public myString = "Hello World!";
+    bytes32 public myBytes32 = "Hello World!";
+    uint256 internal constant Q96 = 0x1000000000000000000000000;
+    uint8 internal constant RESOLUTION = 96;
+    
+
+
+
+
+}
