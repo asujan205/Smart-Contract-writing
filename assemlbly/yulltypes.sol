6d957918c24841dedc8c94698c4249da377d877f
asujan205 <asujan205@gmail.com>
2023-08-06 20:36:59 +0545

    bool

--- assemlbly/yulltypes.sol
+++ assemlbly/yulltypes.sol
@@ -14,4 +14,29 @@ contract YullTypes{
 
     }
 
+    function getHex() public pure returns(uint256){
+     uint256 x;
+      assembly {
+        x := 0x123
+      }
+        return x;
+
+    }
+
+// Zero index bytes32 alwys give false , mean to say   bytes32 zero will alwys returns false
+  function getAssignments() external pure returns(bool) {
+        bool _rep;
+
+        bytes32 zero = bytes32("2");
+
+        assembly {
+            _rep := zero
+        }
+
+        return _rep;
+    }
+
+
+
+
 }
